bd9ec1d522d7086b8529fe8c877a317cda6873d5
Alex-notfound <curras.alex@hotmail.com>
2019-03-13 21:27:39 +0100

    Subida del codigo base para el ejercicio 2

---
+++ CDI_ET6/src/ejercicio2.java
@@ -0,0 +1,99 @@
+/* Ejercicio en el que se utilizara wait() y notifyAll() para que los hilos esperen a que el objeto ClassC 
+sea liberado para utilizarlo y cuando acaben de utilizarlo notifiquen de que han terminado.
+Para llevar un orden de los hilos en la ejecucion del metodo hemos añadido un atributo "puesto" en ClassD y un atributo
+"turno" en ClassC, empleando un metodo de fuerza bruta como exige el enunciado. */
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ejercicio2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        ClassC a = new ClassC();
+        ArrayList<Thread> hilos = new ArrayList<Thread>();
+
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(new ClassD(a, i));
+            thread.start();
+            hilos.add(thread);
+        }
+        //Dormimos por un tiempo para que le de tiempo a los hilos a llegar al wait()
+        //Es una manera cutre, proximamente se explicara una manera mas profesional
+        Thread.sleep(100);
+        synchronized (a) {
+            a.notifyAll();
+        }
+        for (int i = 0; i < hilos.size(); i++) {
+            hilos.get(i).join();
+        }
+        Iterator<Long> coleccion = a.getThreadIds().iterator();
+        System.out.println("-- FINAL -- Contador: " + a.counter);
+        while (coleccion.hasNext()) {
+            System.out.println("Hilo: " + coleccion.next());
+        }
+    }
+}
+
+class ClassC extends Thread {
+
+    int counter;
+    private Set<Long> threadIds;
+    int turno;
+
+    public ClassC() {
+        this.counter = 10;
+        threadIds = new TreeSet<Long>();
+        turno = 0;
+    }
+
+    public Set<Long> getThreadIds() {
+        return threadIds;
+    }
+
+    void EnterAndWait() throws InterruptedException {
+        this.counter--;
+        threadIds.add(Thread.currentThread().getId());
+        System.out.println("Inicia hilo " + Thread.currentThread().getId());
+        Thread.sleep(10);
+        System.out.println("Acaba hilo " + Thread.currentThread().getId());
+        turno++;
+    }
+
+    boolean isFinished() {
+        return this.counter == 0;
+    }
+}
+
+class ClassD implements Runnable {
+
+    private ClassC a;
+    private int puesto;
+
+    public ClassD(ClassC a, int puesto) {
+        this.a = a;
+        this.puesto = puesto;
+    }
+
+    @Override
+    public void run() {
+        try {
+            //Añadimos synchronized para que solo un hilo pueda utilizar el objeto simultaneamente
+            synchronized (a) {
+                while (a.turno != this.puesto) {
+                    a.wait();
+                }
+                if (!a.isFinished()) {
+                    System.out.println("Entra hilo con puesto " + this.puesto);
+                    a.EnterAndWait();
+                }
+                a.notifyAll();
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ClassD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
