505d5f29d0d63aa2de36e51aff84fd8eb4e11316
Alex-notfound <curras.alex@hotmail.com>
2019-03-26 00:09:19 +0100

    Ejercicio 2 comentado

--- CDI_ET7/src/ejercicio2.java
+++ CDI_ET7/src/ejercicio2.java
@@ -1,10 +1,14 @@
+/* En este ejercicio utilizamos el objeto ArrayBlockingQueue para solucionar este problema tipico del Productor/Consumidor.
+Este objeto se encarga de que el hilo se bloquee al intentar agregar un elemento cuando esta lleno, o si intenta
+eliminar un elemento cuando esta vacio. */
 
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class ejercicio2 {
 
     public static void main(String args[]) throws InterruptedException {
-        ArrayBlockingQueue q = new ArrayBlockingQueue<Integer>(1000);
+        //Se crea la instancia del objeto a utilizar para la coherencia entre hilos
+        ArrayBlockingQueue q = new ArrayBlockingQueue<Integer>(100);
         Productor2[] prod = new Productor2[10];
         Consumidor2[] cons = new Consumidor2[10];
         
@@ -40,6 +44,7 @@ class Productor2 extends Thread {
             try {
                 Thread.sleep((long) (Math.random() * 5));
                 n=(int) ((Math.random()*9));
+                //Se introduce el valor aleatorio generado en el buffer si no esta lleno
                 buffer.put(n);
                 System.out.println("Añadido " + n);
             } catch (InterruptedException ex) {
@@ -61,6 +66,7 @@ class Consumidor2 extends Thread {
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep((long) (Math.random() * 5));
+                //Coge un elemento del buffer si no esta vacio
                 System.out.println("Eliminado " + buffer.take());
             } catch (InterruptedException ex) {
                 break;
