e6f0cbe5816a555c8d31e9b20926907793acd974
Alex-notfound <curras.alex@hotmail.com>
2019-03-25 23:11:02 +0100

    Ejercicio 1 comentado

--- CDI_ET7/src/ejercicio1.java
+++ CDI_ET7/src/ejercicio1.java
@@ -1,3 +1,6 @@
+/* En este ejercicio se utiliza ReentrantLock y Conditions, junto a los metodos lock(), await() y signal()
+para resolver el problema tipico del Productor/Consumidor.
+A mayores se ha evitado que un productor añada 2 veces consecutivas */
 
 import java.util.ArrayList;
 import java.util.concurrent.locks.Condition;
@@ -6,16 +9,22 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ejercicio1 {
 
     public static void main(String args[]) throws InterruptedException {
+        
         Buffer buffer = new Buffer();
         Productor[] prod = new Productor[10];
         Consumidor[] cons = new Consumidor[10];
+        
+        //Crea e inicia varios hilos Productores y Consumidores
         for (int i = 0; i < prod.length; i++) {
             prod[i] = new Productor(buffer);
             cons[i] = new Consumidor(buffer);
             prod[i].start();
             cons[i].start();
         }
+        
         Thread.sleep(1000);
+        
+        //Interrumpe todos los hilos ejecutados
         for (int i = 0; i < prod.length; i++) {
             prod[i].interrupt();
             cons[i].interrupt();
@@ -65,13 +74,14 @@ class Consumidor extends Thread {
 }
 
 class Buffer {
-
+    //Se instancian los objetos a utilizar en este ejercicio para asegurar la coherencia
     private final ReentrantLock lock = new ReentrantLock();
     final Condition notFull = lock.newCondition();
     final Condition notEmpty = lock.newCondition();
-    boolean anteriorProductor;
     ArrayList lista;
     int capacidad;
+    //Este booleano es el que determinara si el anterior hilo que utilizo el buffer era un productor
+    boolean anteriorProductor;
 
     public Buffer() {
         lista = new ArrayList<Integer>();
@@ -82,6 +92,7 @@ class Buffer {
     void write() throws InterruptedException {
         lock.lock();
         try {
+            //Mientras el buffer esta lleno o ha sido utilizado por un productor la ultima vez, espera
             while (lista.size() == capacidad || anteriorProductor) {
                 notFull.await();
             }
@@ -89,6 +100,7 @@ class Buffer {
             lista.add(e);
             anteriorProductor = true;
             System.out.println("Añadido " + e);
+            //Avisa de que el buffer ya no esta vacio
             notEmpty.signal();
         } finally {
             lock.unlock();
@@ -98,11 +110,13 @@ class Buffer {
     void read() throws InterruptedException {
         lock.lock();
         try {
+            //Mientras el buffer este vacio, espera
             while (lista.size() == 0) {
                 notEmpty.await();
             }
             anteriorProductor = false;
             System.out.println("Eliminado " + lista.remove(0));
+            //Avisa de que el buffer ya no esta lleno
             notFull.signal();
         } finally {
             lock.unlock();
