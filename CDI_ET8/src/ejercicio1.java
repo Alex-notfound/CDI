5c7bef919b07fc97b3adc7d01eaf8e7c3e581725
Alex-notfound <curras.alex@hotmail.com>
2019-04-01 23:25:36 +0200

    Comentado ejercicio 1

--- CDI_ET8/src/ejercicio1.java
+++ CDI_ET8/src/ejercicio1.java
@@ -1,7 +1,7 @@
 /* 
 En este ejercicio utilizamos la clase Executor de Java, de manera que ejecuta tantos hilos como numero de
 procesadores disponibles para calcular el factorial de varios numeros aleatorios
-*/
+ */
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,15 +9,22 @@ import java.util.concurrent.Executors;
 public class ejercicio1 {
 
     public static void main(String args[]) {
+        //Creacion del executor utilizando un metodo que obtiene el numero de procesadores disponibles
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        System.out.println(Runtime.getRuntime().availableProcessors());
+        //Imprime el numero de procesadores disponibles
+        System.out.println("Numero de procesadores disponibles: " + Runtime.getRuntime().availableProcessors());
+        //Crea un array de tareas
         tarea[] tareas = new tarea[40];
+        //Instancia y el executor ejecuta todas las tareas de manera ordenada
         for (int i = 0; i < tareas.length; i++) {
             tareas[i] = new tarea((int) Math.floor(Math.random() * 10));
             executor.execute(tareas[i]);
         }
+        //Cierra el executor
         executor.shutdown();
+        //Avisa de que el executor se ha cerrado
         System.out.println("Ejecutor cerrado");
+        //EL programa continua su ejecucion mientras el executor no termine
         while (!executor.isTerminated()) {
         }
     }
@@ -27,10 +34,12 @@ class tarea implements Runnable {
 
     int num;
 
+    //Constructor de la tarea, a la cual se le pasa como parametro un numero para calcular su factorial
     public tarea(int num) {
         this.num = num;
     }
 
+    //Calcula el factorial de un numero
     public void run() {
         int result = 1;
         if (num > 1) {
@@ -43,6 +52,7 @@ class tarea implements Runnable {
                 }
             }
         }
+        //Imprime el resultado
         System.out.println("El factorial de " + num + " es: " + result);
     }
 }
